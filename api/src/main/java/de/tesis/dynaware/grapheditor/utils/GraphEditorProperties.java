0d19524af171970d32e6014a90e6c4f94206273b
rmfisher <fisher@tum.de>
2015-01-21 19:29:33 +1300

    Changed custom properties map to observable map

--- api/src/main/java/de/tesis/dynaware/grapheditor/utils/GraphEditorProperties.java
+++ api/src/main/java/de/tesis/dynaware/grapheditor/utils/GraphEditorProperties.java
@@ -10,6 +10,8 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
 
 /**
  * General properties for the graph editor.
@@ -55,7 +57,7 @@ public class GraphEditorProperties {
     private final BooleanProperty snapToGridProperty = new SimpleBooleanProperty();
     private final DoubleProperty gridSpacingProperty = new SimpleDoubleProperty(DEFAULT_GRID_SPACING);
 
-    private final Map<String, String> customProperties = new HashMap<>();
+    private final ObservableMap<String, String> customProperties = FXCollections.observableHashMap();
 
     /**
      * Creates a new editor properties instance containing a set of default properties.
