ede07b780485f41180489465f7b65c3d97bb199b
eckig <srachner+github@gmail.com>
2016-12-14 16:07:40 +0100

    GraphEditorGrid: Use custom Background instead of many Line objects

--- core/src/main/java/de/tesis/dynaware/grapheditor/core/view/GraphEditorGrid.java
+++ core/src/main/java/de/tesis/dynaware/grapheditor/core/view/GraphEditorGrid.java
@@ -7,23 +7,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import de.tesis.dynaware.grapheditor.core.DefaultGraphEditor;
+import de.tesis.dynaware.grapheditor.utils.GraphEditorProperties;
 import javafx.css.CssMetaData;
 import javafx.css.StyleConverter;
 import javafx.css.Styleable;
 import javafx.css.StyleableObjectProperty;
 import javafx.css.StyleableProperty;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import de.tesis.dynaware.grapheditor.core.DefaultGraphEditor;
-import de.tesis.dynaware.grapheditor.utils.GraphEditorProperties;
-import javafx.scene.Group;
-import javafx.scene.shape.Line;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 
 /**
  * The alignment grid that appears in the background of the editor.
  */
-public class GraphEditorGrid extends Group {
+public class GraphEditorGrid extends Region {
 
     private static final String STYLE_CLASS = "graph-editor-grid";
     private static final String GRID_COLOR_SELECTOR = "-grid-color";
@@ -33,10 +38,10 @@ public class GraphEditorGrid extends Group {
 
     private GraphEditorProperties editorProperties;
 
-    private final StyleableObjectProperty<Paint> gridColor = new StyleableObjectProperty<Paint>(DEFAULT_GRID_COLOR) {
+    private final StyleableObjectProperty<Color> gridColor = new StyleableObjectProperty<Color>(DEFAULT_GRID_COLOR) {
 
         @Override
-        public CssMetaData<? extends Styleable, Paint> getCssMetaData() {
+        public CssMetaData<? extends Styleable, Color> getCssMetaData() {
             return StyleableProperties.GRID_COLOR;
         }
 
@@ -49,22 +54,39 @@ public class GraphEditorGrid extends Group {
         public String getName() {
             return GRID_COLOR_PROPERTY_NAME;
         }
-
+        
         @Override
         protected void invalidated() {
-            requestLayout();
+        	updateGrid();
         }
     };
 
     /**
-     * Creates a new grid manager. Only one instance should exist per {@link DefaultGraphEditor} instance.
+     * Creates a new grid manager. Only one instance should exist per
+     * {@link DefaultGraphEditor} instance.
      */
     public GraphEditorGrid() {
 
-        // The grid should under NO circumstances interfere with (a) the layout of its parent, or (b) mouse events.
-        setManaged(false);
         setMouseTransparent(true);
         getStyleClass().add(STYLE_CLASS);
+        updateGrid();
+    }
+    
+	private void updateGrid() {
+		final Color color = gridColor.get();
+		final double space = editorProperties == null ? GraphEditorProperties.DEFAULT_GRID_SPACING
+				: editorProperties.getGridSpacing();
+		final LinearGradient lgH = new LinearGradient(0, 0, 0, space, false, CycleMethod.REPEAT, new Stop(0, color),
+				new Stop(0.0625, color), new Stop(0.0625, Color.TRANSPARENT), new Stop(1, Color.TRANSPARENT));
+		final LinearGradient lgV = new LinearGradient(0, 0, space, 0, false, CycleMethod.REPEAT, new Stop(0, color),
+				new Stop(0.0625, color), new Stop(0.0625, Color.TRANSPARENT), new Stop(1, Color.TRANSPARENT));
+		setBackground(new Background(new BackgroundFill(lgH, CornerRadii.EMPTY, Insets.EMPTY),
+				new BackgroundFill(lgV, CornerRadii.EMPTY, Insets.EMPTY)));
+	}
+
+    @Override
+    protected void layoutChildren() {
+    	// empty
     }
 
     /**
@@ -73,74 +95,10 @@ public class GraphEditorGrid extends Group {
      * @param editorProperties a {@link GraphEditorProperties} instance
      */
     public void setProperties(final GraphEditorProperties editorProperties) {
+        visibleProperty().unbind();
         this.editorProperties = editorProperties;
-        
         if(this.editorProperties != null) {
-            visibleProperty().bind(this.editorProperties.gridVisibleProperty());
-        }
-        else {
-            visibleProperty().unbind();
-        }
-    }
-
-    @Override
-    protected void layoutChildren() {
-        super.layoutChildren();
-        
-        // Unless this Grid is resized, there is no need to repaint everything.. just adjust the color:
-        for(final Node child : getChildren()) {
-            if(child instanceof Line) {
-                ((Line)child).setStroke(gridColor.get());
-            }
-        }
-    }
-
-    @Override
-    public void resize(double width, double height) {
-        super.resize(width, height);
-        draw(width, height);
-    }
-
-    /**
-     * Draws the grid for the given width and height.
-     *
-     * @param width the width of the editor region
-     * @param height the height of the editor region
-     */
-    private void draw(final double width, final double height) {
-        
-        getChildren().clear();
-
-        final double spacing = editorProperties.getGridSpacing();
-        final int hLineCount = (int) Math.floor((height + 1) / spacing);
-        final int vLineCount = (int) Math.floor((width + 1) / spacing);
-
-        for (int i = 0; i < hLineCount; i++) {
-
-            final Line hLine = new Line();
-            final double y = (i + 1) * spacing - 1;
-
-            hLine.setStartX(0);
-            hLine.setEndX(width);
-            hLine.setStartY(y);
-            hLine.setEndY(y);
-            hLine.setStroke(gridColor.get());
-
-            getChildren().add(hLine);
-        }
-
-        for (int i = 0; i < vLineCount; i++) {
-
-            final Line vLine = new Line();
-            final double x = (i + 1) * spacing - 1;
-
-            vLine.setStartX(x);
-            vLine.setEndX(x);
-            vLine.setStartY(0);
-            vLine.setEndY(height);
-            vLine.setStroke(gridColor.get());
-
-            getChildren().add(vLine);
+            visibleProperty().bind(editorProperties.gridVisibleProperty());
         }
     }
 
@@ -156,28 +114,28 @@ public class GraphEditorGrid extends Group {
      * Currently only the grid color is styled in this way.
      * </p>
      */
-    private static class StyleableProperties {
+	private static class StyleableProperties {
 
-        private static final CssMetaData<GraphEditorGrid, Paint> GRID_COLOR = new CssMetaData<GraphEditorGrid, Paint>(
-                GRID_COLOR_SELECTOR, StyleConverter.getPaintConverter()) {
+		private static final CssMetaData<GraphEditorGrid, Color> GRID_COLOR = new CssMetaData<GraphEditorGrid, Color>(
+				GRID_COLOR_SELECTOR, StyleConverter.getColorConverter()) {
 
-            @Override
-            public boolean isSettable(final GraphEditorGrid node) {
-                return !node.gridColor.isBound();
-            }
+			@Override
+			public boolean isSettable(final GraphEditorGrid node) {
+				return !node.gridColor.isBound();
+			}
 
-            @Override
-            public StyleableProperty<Paint> getStyleableProperty(final GraphEditorGrid node) {
-                return node.gridColor;
-            }
-        };
+			@Override
+			public StyleableProperty<Color> getStyleableProperty(final GraphEditorGrid node) {
+				return node.gridColor;
+			}
+		};
 
-        private static final List<CssMetaData<? extends Styleable, ?>> STYLEABLES;
+		private static final List<CssMetaData<? extends Styleable, ?>> STYLEABLES;
 
-        static {
-            final List<CssMetaData<? extends Styleable, ?>> styleables = new ArrayList<>(Node.getClassCssMetaData());
-            styleables.add(GRID_COLOR);
-            STYLEABLES = Collections.unmodifiableList(styleables);
-        }
-    }
+		static {
+			final List<CssMetaData<? extends Styleable, ?>> styleables = new ArrayList<>(Node.getClassCssMetaData());
+			styleables.add(GRID_COLOR);
+			STYLEABLES = Collections.unmodifiableList(styleables);
+		}
+	}
 }
