d81d7cd59b1959acceec35ecb12966edc5d800dc
eckig <srachner+github@gmail.com>
2019-06-10 20:06:32 +0200

    WIP: Layout with Eclipse ELK

--- demo/src/main/java/de/tesis/dynaware/grapheditor/demo/GraphEditorDemoController.java
+++ demo/src/main/java/de/tesis/dynaware/grapheditor/demo/GraphEditorDemoController.java
@@ -3,16 +3,43 @@
  */
 package de.tesis.dynaware.grapheditor.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import de.tesis.dynaware.grapheditor.*;
+import de.tesis.dynaware.grapheditor.core.connectors.DefaultConnectorTypes;
+import de.tesis.dynaware.grapheditor.model.*;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import org.eclipse.elk.alg.disco.DisCoLayoutProvider;
+import org.eclipse.elk.alg.layered.LayeredLayoutProvider;
+import org.eclipse.elk.alg.mrtree.TreeLayoutProvider;
+import org.eclipse.elk.alg.radial.RadialLayoutProvider;
+import org.eclipse.elk.alg.spore.OverlapRemovalLayoutProvider;
+import org.eclipse.elk.alg.spore.ShrinkTreeLayoutProvider;
+import org.eclipse.elk.core.AbstractLayoutProvider;
+import org.eclipse.elk.core.IGraphLayoutEngine;
+import org.eclipse.elk.core.data.*;
+import org.eclipse.elk.core.math.ElkMargin;
+import org.eclipse.elk.core.math.ElkPadding;
+import org.eclipse.elk.core.math.KVector;
+import org.eclipse.elk.core.math.KVectorChain;
+import org.eclipse.elk.core.options.*;
+import org.eclipse.elk.core.util.BasicProgressMonitor;
+import org.eclipse.elk.core.util.BoxLayoutProvider;
+import org.eclipse.elk.core.util.IElkProgressMonitor;
+import org.eclipse.elk.graph.*;
+import org.eclipse.elk.graph.properties.IPropertyHolder;
+import org.eclipse.elk.graph.util.ElkReflect;
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
-import de.tesis.dynaware.grapheditor.Commands;
-import de.tesis.dynaware.grapheditor.GraphEditor;
 import de.tesis.dynaware.grapheditor.core.DefaultGraphEditor;
 import de.tesis.dynaware.grapheditor.core.skins.defaults.connection.SimpleConnectionSkin;
 import de.tesis.dynaware.grapheditor.core.view.GraphEditorContainer;
@@ -25,21 +52,12 @@ import de.tesis.dynaware.grapheditor.demo.customskins.tree.TreeConnectorValidato
 import de.tesis.dynaware.grapheditor.demo.customskins.tree.TreeSkinConstants;
 import de.tesis.dynaware.grapheditor.demo.selections.SelectionCopier;
 import de.tesis.dynaware.grapheditor.demo.utils.AwesomeIcon;
-import de.tesis.dynaware.grapheditor.model.GModel;
-import de.tesis.dynaware.grapheditor.model.GNode;
-import de.tesis.dynaware.grapheditor.model.GraphFactory;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.SetChangeListener;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -95,6 +113,8 @@ public class GraphEditorDemoController {
     private ToggleButton minimapButton;
     @FXML
     private GraphEditorContainer graphEditorContainer;
+    @FXML
+    private ToolBar bottomToolbar;
 
     private final GraphEditor graphEditor = new DefaultGraphEditor();
 	private final SelectionCopier selectionCopier = new SelectionCopier(graphEditor.getSkinLookup(),
@@ -136,11 +156,253 @@ public class GraphEditorDemoController {
 
         activeSkinController.set(defaultSkinController);
 
-		graphEditor.modelProperty().addListener((w, o, n) -> selectionCopier.initialize(n));
+        graphEditor.modelProperty().addListener((w, o, n) -> selectionCopier.initialize(n));
         selectionCopier.initialize(model);
 
         initializeMenuBar();
         addActiveSkinControllerListener();
+
+        ElkReflect.register(LinkedList.class, LinkedList::new, p -> ((LinkedList) p).clone());
+        ElkReflect.register(ElkPadding.class, ElkPadding::new, p -> ((ElkPadding) p).clone());
+        ElkReflect.register(ElkMargin.class, ElkMargin::new, p -> ((ElkMargin) p).clone());
+        ElkReflect.register(KVector.class, KVector::new, p -> ((KVector) p).clone());
+        ElkReflect.register(KVectorChain.class, KVectorChain::new, p -> ((KVectorChain) p).clone());
+
+        final CoreOptions options = new CoreOptions();
+        final LayoutMetaDataProviderRegistry registry = new LayoutMetaDataProviderRegistry();
+        options.apply(registry);
+        final ComboBox<IGraphLayoutEngine> layoutEngines = new ComboBox<>(FXCollections.observableArrayList(
+                new LayeredLayoutProvider(), new BoxLayoutProvider(), new TreeLayoutProvider(),
+                new DisCoLayoutProvider(), new RadialLayoutProvider(),
+                new OverlapRemovalLayoutProvider(), new ShrinkTreeLayoutProvider()));
+        layoutEngines.valueProperty().addListener((w, o, n) -> autoLayout(registry, n));
+        bottomToolbar.getItems().add(layoutEngines);
+        registry.addTo(obs -> autoLayout(registry, layoutEngines.getValue()), bottomToolbar.getItems());
+    }
+
+    private void autoLayout(final LayoutMetaDataProviderRegistry pRegistry, final IGraphLayoutEngine pEngine)
+    {
+        if(pEngine==null)
+        {
+            return;
+        }
+
+        final IElkProgressMonitor progress = new BasicProgressMonitor();
+        final Mapping mapping = new Mapping();
+        final ElkNode elkNodeRepresentation = createElkNodeRepresentation(pRegistry, mapping);
+        pEngine.layout(elkNodeRepresentation, progress);
+
+        mapping.apply(graphEditor.getModel());
+        graphEditor.reload();
+    }
+
+    private void setCommonProperties(final LayoutMetaDataProviderRegistry pRegistry, final LayoutOptionData.Target pTarget, final IPropertyHolder pHolder)
+    {
+        pHolder.setProperty(CoreOptions.PORT_CONSTRAINTS, PortConstraints.FIXED_POS);
+        pHolder.setProperty(CoreOptions.ANIMATE, false);
+        pHolder.setProperty(CoreOptions.NODE_SIZE_CONSTRAINTS, SizeConstraint.fixed());
+        pHolder.setProperty(CoreOptions.EDGE_ROUTING, EdgeRouting.ORTHOGONAL);
+        pRegistry.applyTo(pTarget, pHolder);
+    }
+
+    private ElkNode createElkNodeRepresentation(final LayoutMetaDataProviderRegistry pRegistry, final Mapping pMapping)
+    {
+        final ElkNode model = ElkGraphFactory.eINSTANCE.createElkNode();
+        GModel gModel = graphEditor.getModel();
+        if (gModel != null)
+        {
+            if(!gModel.getNodes().isEmpty())
+            {
+                model.setWidth(gModel.getContentWidth());
+                model.setHeight(gModel.getContentHeight());
+                for (final GNode node : gModel.getNodes())
+                {
+                    model.getChildren().add(nodeToElk(pRegistry, node, pMapping));
+                }
+            }
+            if(!gModel.getConnections().isEmpty())
+            {
+                for(final GConnection connection : gModel.getConnections())
+                {
+                    final GConnectionSkin connectionSkin = graphEditor.getSkinLookup().lookupConnection(connection);
+                    if(connectionSkin instanceof VirtualSkin)
+                    {
+                        continue;
+                    }
+                    final ElkEdge edge = ElkGraphFactory.eINSTANCE.createElkEdge();
+                    setCommonProperties(pRegistry, LayoutOptionData.Target.EDGES, edge);
+
+                    final ElkPort portSource = pMapping.connectors.get(connection.getSource());
+                    final ElkPort portTarget = pMapping.connectors.get(connection.getTarget());
+                    if (portSource != null && portTarget != null)
+                    {
+                        edge.getTargets().add(portTarget);
+                        edge.getSources().add(portSource);
+                        pMapping.connections.put(connection, edge);
+                    }
+                }
+            }
+        }
+
+        setCommonProperties(pRegistry, LayoutOptionData.Target.PARENTS, model);
+
+        return model;
+    }
+
+    private static class Mapping
+    {
+        // GConnector  -> ElkPort
+        // GConnection -> ElkEdge
+        // GNode       -> ElkNode
+        // GJoint      -> ElkEdgeSection
+        final Map<GConnection, ElkEdge> connections = new IdentityHashMap<>();
+        final Map<GConnector, ElkPort> connectors = new IdentityHashMap<>();
+        final Map<GNode, ElkNode> nodes = new IdentityHashMap<>();
+
+        public void apply(final GModel pModel)
+        {
+            final EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(pModel);
+            final CompoundCommand command = new CompoundCommand();
+
+            for(final Map.Entry<GNode, ElkNode> entry : nodes.entrySet())
+            {
+                final GNode node = entry.getKey();
+                final ElkNode eNode = entry.getValue();
+                command.append(SetCommand.create(editingDomain, node, GraphPackage.Literals.GNODE__X, eNode.getX()));
+                command.append(SetCommand.create(editingDomain, node, GraphPackage.Literals.GNODE__Y, eNode.getY()));
+            }
+
+            for(final Map.Entry<GConnection, ElkEdge> entry : connections.entrySet())
+            {
+                final GConnection connection = entry.getKey();
+                final List<GJoint> newJoints = new ArrayList<>();
+                for(final ElkEdgeSection section : entry.getValue().getSections())
+                {
+                    for(final ElkBendPoint point : section.getBendPoints())
+                    {
+                        final GJoint joint = GraphFactory.eINSTANCE.createGJoint();
+                        joint.setX(point.getX());
+                        joint.setY(point.getY());
+                        newJoints.add(joint);
+                    }
+                }
+                command.append(SetCommand.create(editingDomain, connection, GraphPackage.Literals.GCONNECTION__JOINTS, newJoints));
+            }
+
+            if (command.canExecute())
+            {
+                command.execute();
+            }
+        }
+    }
+
+    private ElkNode nodeToElk(final LayoutMetaDataProviderRegistry pRegistry, final GNode pNode, final Mapping pMapping)
+    {
+        final ElkNode model = ElkGraphFactory.eINSTANCE.createElkNode();
+        model.setWidth(pNode.getWidth());
+        model.setHeight(pNode.getHeight());
+        model.setX(pNode.getX());
+        model.setY(pNode.getY());
+        model.setIdentifier(pNode.getId());
+        setCommonProperties(pRegistry, LayoutOptionData.Target.NODES, model);
+
+        final GNodeSkin skin = graphEditor.getSkinLookup().lookupNode(pNode);
+        skin.layoutConnectors();
+
+        for(final GConnector connector : pNode.getConnectors())
+        {
+            final ElkPort port = ElkGraphFactory.eINSTANCE.createElkPort();
+            final GConnectorSkin connectorSkin = graphEditor.getSkinLookup().lookupConnector(connector);
+            port.setX(connectorSkin.getRoot().getLayoutX());
+            port.setY(connectorSkin.getRoot().getLayoutY());
+            port.setWidth(connectorSkin.getWidth());
+            port.setHeight(connectorSkin.getHeight());
+            port.setIdentifier(connector.getId());
+            setCommonProperties(pRegistry, LayoutOptionData.Target.PORTS, port);
+
+            final Side side = DefaultConnectorTypes.getSide(connector.getType());
+            if(side!=null)
+            {
+                switch (side)
+                {
+                    case TOP:
+                        port.setProperty(CoreOptions.PORT_SIDE, PortSide.NORTH);
+                        break;
+                    case LEFT:
+                        port.setProperty(CoreOptions.PORT_SIDE, PortSide.WEST);
+                        break;
+                    case RIGHT:
+                        port.setProperty(CoreOptions.PORT_SIDE, PortSide.EAST);
+                        break;
+                    case BOTTOM:
+                        port.setProperty(CoreOptions.PORT_SIDE, PortSide.SOUTH);
+                        break;
+                }
+            }
+
+            model.getPorts().add(port);
+            pMapping.connectors.put(connector, port);
+        }
+
+        pMapping.nodes.put(pNode, model);
+        return model;
+    }
+
+    private static class LayoutMetaDataProviderRegistry implements ILayoutMetaDataProvider.Registry {
+
+        private final Map<LayoutOptionData, ComboBox<String>> imLayoutOptionData = new HashMap<>();
+        private final Map<String, LayoutAlgorithmData> imLayouts = new HashMap<>();
+        private final ComboBox<String> imCbxLayouts = new ComboBox<>();
+
+        @Override
+        public void register(LayoutAlgorithmData layoutAlgorithmData) {
+            imLayouts.put(layoutAlgorithmData.getName(), layoutAlgorithmData);
+            imCbxLayouts.getItems().add(layoutAlgorithmData.getName());
+        }
+
+        @Override
+        public void register(LayoutOptionData layoutOptionData) {
+            final String[] opts = layoutOptionData.getChoices();
+            if (opts != null && opts.length > 0) {
+                final ComboBox<String> cbx = new ComboBox<>(FXCollections.observableArrayList(layoutOptionData.getChoices()));
+                imLayoutOptionData.put(layoutOptionData, cbx);
+            }
+        }
+
+        @Override
+        public void register(LayoutCategoryData layoutCategoryData) {
+
+        }
+
+        @Override
+        public void addDependency(String s, String s1, Object o) {
+
+        }
+
+        @Override
+        public void addOptionSupport(String s, String s1, Object o) {
+
+        }
+
+        public void addTo(final InvalidationListener pListener, final ObservableList<Node> pParent) {
+            imLayoutOptionData.forEach((k, v) -> pParent.add(new HBox(new Label(k.getName()), v)));
+            imLayoutOptionData.values().forEach(n -> n.valueProperty().addListener(pListener));
+        }
+
+        public void applyTo(final LayoutOptionData.Target pTarget, final IPropertyHolder pHolder) {
+            for (final Map.Entry<LayoutOptionData, ComboBox<String>> entry : imLayoutOptionData.entrySet()) {
+                final LayoutOptionData key = entry.getKey();
+                final ComboBox<String> value = entry.getValue();
+//                if (key.getTargets().contains(pTarget) && value.getValue() != null) {
+                    pHolder.setProperty(key, key.parseValue(value.getValue()));
+//                }
+            }
+        }
+
+        public AbstractLayoutProvider fetchLayout() {
+            final String selected = imCbxLayouts.getValue();
+            return selected != null ? imLayouts.get(selected).getInstancePool().fetch() : null;
+        }
     }
 
     @FXML
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -10,6 +10,14 @@ module fx.graph.editor.demo
     requires org.eclipse.emf.common;
     requires org.eclipse.emf.ecore.xmi;
     requires org.eclipse.emf.edit;
+    requires org.eclipse.elk.graph;
+    requires org.eclipse.elk.core;
+    requires org.eclipse.elk.alg.mrtree;
+    requires org.eclipse.elk.alg.layered;
+    requires org.eclipse.elk.alg.disco;
+    requires org.eclipse.elk.alg.radial;
+    requires org.eclipse.elk.alg.spore;
+    requires com.google.common;
     requires slf4j.api;
 
     exports de.tesis.dynaware.grapheditor.demo to javafx.graphics, javafx.fxml;
