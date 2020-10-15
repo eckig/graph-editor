681e1cbf19cca25205205df6d89cc9a55cd39d0d
daniele.maddaluno <daniele.maddaluno@gmail.com>
2020-10-15 12:05:31 +0200

    Introduced JPackageScriptFX for the demo project

---
+++ demo/build_app.bat
@@ -0,0 +1,99 @@
+@ECHO OFF
+
+# Replaced: "main-ui-" --> "graph-editor-demo-"
+# Replaced: "target\classes\com\dlsc\jpackagefx\App.class" --> "target\classes\de\tesis\dynaware\grapheditor\demo\GraphEditorDemo.class"
+# Replaced: "JPackageScriptFX" --> "GraphEditorDemo"
+# Replaced: "com.dlsc.jpackagefx.AppLauncher" --> "de.tesis.dynaware.grapheditor.demo.GraphEditorDemo"
+# Replaced: "src/main/logo/" --> "src/main/resources/de/tesis/dynaware/grapheditor/demo/logo/"
+# Replaced: "JPackageScriptFX" --> "GraphEditorDemo"
+
+rem ------ ENVIRONMENT --------------------------------------------------------
+rem The script depends on various environment variables to exist in order to
+rem run properly. The java version we want to use, the location of the java
+rem binaries (java home), and the project version as defined inside the pom.xml
+rem file, e.g. 1.0-SNAPSHOT.
+rem
+rem PROJECT_VERSION: version used in pom.xml, e.g. 1.0-SNAPSHOT
+rem APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
+
+set JAVA_VERSION=14
+set MAIN_JAR=graph-editor-demo-%PROJECT_VERSION%.jar
+
+rem Set desired installer type: "app-image" "msi" "exe".
+set INSTALLER_TYPE=msi
+
+rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
+rem Remove previously generated java runtime and installers. Copy all required
+rem jar files into the input/libs folder.
+
+IF EXIST target\java-runtime rmdir /S /Q  .\target\java-runtime
+IF EXIST target\installer rmdir /S /Q target\installer
+
+xcopy /S /Q target\libs\* target\installer\input\libs\
+copy target\%MAIN_JAR% target\installer\input\libs\
+
+rem ------ REQUIRED MODULES ---------------------------------------------------
+rem Use jlink to detect all modules that are required to run the application.
+rem Starting point for the jdep analysis is the set of jars being used by the
+rem application.
+
+echo detecting required modules
+
+"%JAVA_HOME%\bin\jdeps" ^
+  -q ^
+  --multi-release %JAVA_VERSION% ^
+  --ignore-missing-deps ^
+  --class-path "target\installer\input\libs\*" ^
+  --print-module-deps target\classes\de\tesis\dynaware\grapheditor\demo\GraphEditorDemo.class > temp.txt
+
+set /p detected_modules=<temp.txt
+
+echo detected modules: %detected_modules%
+
+rem ------ MANUAL MODULES -----------------------------------------------------
+rem jdk.crypto.ec has to be added manually bound via --bind-services or
+rem otherwise HTTPS does not work.
+rem
+rem See: https://bugs.openjdk.java.net/browse/JDK-8221674
+
+set manual_modules=jdk.crypto.ec
+echo manual modules: %manual_modules%
+
+rem ------ RUNTIME IMAGE ------------------------------------------------------
+rem Use the jlink tool to create a runtime image for our application. We are
+rem doing this is a separate step instead of letting jlink do the work as part
+rem of the jpackage tool. This approach allows for finer configuration and also
+rem works with dependencies that are not fully modularized, yet.
+
+echo creating java runtime image
+
+call "%JAVA_HOME%\bin\jlink" ^
+  --strip-native-commands ^
+  --no-header-files ^
+  --no-man-pages ^
+  --compress=2 ^
+  --strip-debug ^
+  --add-modules %detected_modules%,%manual_modules% ^
+  --output target/java-runtime
+
+
+rem ------ PACKAGING ----------------------------------------------------------
+rem In the end we will find the package inside the target/installer directory.
+
+call "%JAVA_HOME%\bin\jpackage" ^
+  --type %INSTALLER_TYPE% ^
+  --dest target/installer ^
+  --input target/installer/input/libs ^
+  --name GraphEditorDemo ^
+  --main-class de.tesis.dynaware.grapheditor.demo.GraphEditorDemo ^
+  --main-jar %MAIN_JAR% ^
+  --java-options -Xmx2048m ^
+  --runtime-image target/java-runtime ^
+  --icon src/main/resources/de/tesis/dynaware/grapheditor/demo/logo/windows/duke.ico ^
+  --app-version %APP_VERSION% ^
+  --vendor "ACME Inc." ^
+  --copyright "Copyright © 2019-20 ACME Inc." ^
+  --win-dir-chooser ^
+  --win-shortcut ^
+  --win-per-user-install ^
+  --win-menu
---
+++ demo/build_app.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Replaced: "main-ui-" --> "graph-editor-demo-"
+# Replaced: "target/classes/com/dlsc/jpackagefx/App.class" --> "target/classes/de/tesis/dynaware/grapheditor/demo/GraphEditorDemo.class"
+# Replaced: "JPackageScriptFX" --> "GraphEditorDemo"
+# Replaced: "com.dlsc.jpackagefx.AppLauncher" --> "de.tesis.dynaware.grapheditor.demo.GraphEditorDemo"
+# Replaced: "src/main/logo/" --> "src/main/resources/de/tesis/dynaware/grapheditor/demo/logo/"
+# Replaced: "JPackageScriptFX" --> "GraphEditorDemo"
+
+# ------ ENVIRONMENT --------------------------------------------------------
+# The script depends on various environment variables to exist in order to
+# run properly. The java version we want to use, the location of the java
+# binaries (java home), and the project version as defined inside the pom.xml
+# file, e.g. 1.0-SNAPSHOT.
+#
+# PROJECT_VERSION: version used in pom.xml, e.g. 1.0-SNAPSHOT
+# APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
+
+JAVA_VERSION=14
+MAIN_JAR="graph-editor-demo-$PROJECT_VERSION.jar"
+
+# Set desired installer type: "app-image", "dmg", "pkg", "rpm" or "deb".
+INSTALLER_TYPE=app-image
+
+echo "java home: $JAVA_HOME"
+echo "project version: $PROJECT_VERSION"
+echo "app version: $APP_VERSION"
+echo "main JAR file: $MAIN_JAR"
+
+# ------ SETUP DIRECTORIES AND FILES ----------------------------------------
+# Remove previously generated java runtime and installers. Copy all required
+# jar files into the input/libs folder.
+
+rm -rfd ./target/java-runtime/
+rm -rfd target/installer/
+
+mkdir -p target/installer/input/libs/
+
+cp target/libs/* target/installer/input/libs/
+cp target/${MAIN_JAR} target/installer/input/libs/
+
+# ------ REQUIRED MODULES ---------------------------------------------------
+# Use jlink to detect all modules that are required to run the application.
+# Starting point for the jdep analysis is the set of jars being used by the
+# application.
+
+echo "detecting required modules"
+detected_modules=`$JAVA_HOME/bin/jdeps \
+  -q \
+  --multi-release ${JAVA_VERSION} \
+  --ignore-missing-deps \
+  --print-module-deps \
+  --class-path "target/installer/input/libs/*" \
+    target/classes/de/tesis/dynaware/grapheditor/demo/GraphEditorDemo.class`
+echo "detected modules: ${detected_modules}"
+
+
+# ------ MANUAL MODULES -----------------------------------------------------
+# jdk.crypto.ec has to be added manually bound via --bind-services or
+# otherwise HTTPS does not work.
+#
+# See: https://bugs.openjdk.java.net/browse/JDK-8221674
+#
+# In addition we need jdk.localedata if the application is localized.
+# This can be reduced to the actually needed locales via a jlink paramter,
+# e.g., --include-locales=en,de.
+
+manual_modules=jdk.crypto.ec,jdk.localedata
+echo "manual modules: ${manual_modules}"
+
+# ------ RUNTIME IMAGE ------------------------------------------------------
+# Use the jlink tool to create a runtime image for our application. We are
+# doing this is a separate step instead of letting jlink do the work as part
+# of the jpackage tool. This approach allows for finer configuration and also
+# works with dependencies that are not fully modularized, yet.
+
+echo "creating java runtime image"
+$JAVA_HOME/bin/jlink \
+  --strip-native-commands \
+  --no-header-files \
+  --no-man-pages  \
+  --compress=2  \
+  --strip-debug \
+  --add-modules "${detected_modules},${manual_modules}" \
+  --include-locales=en,de \
+  --output target/java-runtime
+
+# ------ PACKAGING ----------------------------------------------------------
+# In the end we will find the package inside the target/installer directory.
+
+echo "Creating installer of type $INSTALLER_TYPE"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+$JAVA_HOME/bin/jpackage \
+--type $INSTALLER_TYPE \
+--dest target/installer \
+--input target/installer/input/libs \
+--name GraphEditorDemo \
+--main-class de.tesis.dynaware.grapheditor.demo.GraphEditorDemo \
+--main-jar ${MAIN_JAR} \
+--java-options -Xmx2048m \
+--runtime-image target/java-runtime \
+--icon src/main/resources/de/tesis/dynaware/grapheditor/demo/logo/linux/duke.png \
+--app-version ${APP_VERSION} \
+--vendor "ACME Inc." \
+--copyright "Copyright © 2019-20 ACME Inc."
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+$JAVA_HOME/bin/jpackage \
+--type $INSTALLER_TYPE \
+--dest target/installer \
+--input target/installer/input/libs \
+--name GraphEditorDemo \
+--main-class de.tesis.dynaware.grapheditor.demo.GraphEditorDemo \
+--main-jar ${MAIN_JAR} \
+--java-options -Xmx2048m \
+--runtime-image target/java-runtime \
+--icon src/main/resources/de/tesis/dynaware/grapheditor/demo/logo/macosx/duke.icns \
+--app-version "1.0"
+# ${APP_VERSION} \
+# --vendor "ACME Inc." \
+# --copyright "Copyright © 2019-20 ACME Inc." \
+# --mac-package-identifier com.acme.app \
+# --mac-package-name ACME
+
+else
+    echo "Unknown OSTYPE: $OSTYPE"
+    exit 1
+fi
